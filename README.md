1cd6ec800cf83ae1022efd6070c61dd352b25534
Allz <278137257@qq.com>
2021-10-30 01:22:56 +0800

    重构代码

---
+++ README.md
@@ -0,0 +1,19 @@
+# EnableMilkyWayGalaxy
+
+## Features
+Enables uploading cluster generation to galaxy server even if you use mods or not your game save.
+Limited this version(0.8.22) available
+
+即使使用mod 或者 使用其他人的存档 也允许上传戴森球发电数据到银河系服务器。
+限游戏版本 （0.8.22） 可用
+
+## Changelog
+
+### v1.0.2
+- Refactor the code
+
+### v1.0.1
+- Updated to work with game version 0.8.22.9331 or higher
+
+### v1.0.0
+- Initial release
---
+++ patch/Patches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace EnableMilkyWayGalaxy.patch
+{
+    [HarmonyPatch]
+    public class Patches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GameAbnormalityCheck_Obsolete), "isGameNormal")]
+        public static bool IsGameNormalPatch() => true;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PARTNER), "UploadClusterGenerationToGalaxyServer")]
+        public static void UploadClusterGenerationToGalaxyServer(GameData gameData)
+        {
+            PartnerPatches.UploadClusterGenerationToGalaxyServer(gameData);
+        }
+    }
+}

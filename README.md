24e07d3447843b63603fc32370a0db325566d15e
Allz <278137257@qq.com>
2021-10-30 12:40:26 +0800

    1、添加使用mod或者其他人的存档也可以解锁成就功能 2、优化部分代码

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # EnableMilkyWayGalaxy
 
 ## Features
-Enables uploading cluster generation to galaxy server even if you use mods or not your game save.
-Limited this version(0.8.22) available
+- Enables unlocking achievements and uploading cluster generation to galaxy server even if you use mods or not your game save.
 
-即使使用mod 或者 使用其他人的存档 也允许上传戴森球发电数据到银河系服务器。
-限游戏版本 （0.8.22） 可用
+- Limited this version(0.8.22) available
+
+- 即使使用 mod / 其他人的存档 也允许 解锁成就、上传戴森球发电数据到银河系服务器。
+
+- 限游戏版本 （0.8.22） 可用
 
 ## Changelog
 
 ### v1.0.2
+- Enables unlocking achievements even if mods are used now.
 - Refactor the code
 
 ### v1.0.1
--- patches/Patches.cs
+++ patches/Patches.cs
@@ -6,14 +6,27 @@ namespace EnableMilkyWayGalaxy.patches
     public class Patches
     {
         /*
-         * [HarmonyPostfix] 在 GameSave.SaveCurrentGame()后执行
-         * SaveCurrentGame()方法中会判断游戏数据合法性，只有当游戏数据异常未执行数据上传时，则该补丁会将数据合理化并上传
+         * 该补丁会处理戴森球发电数据使其合理化并上传至银河系服务器
          */
-        [HarmonyPostfix]
+        [HarmonyPrefix]
         [HarmonyPatch(typeof(GameSave), "SaveCurrentGame")]
-        public static void SaveCurrentGamePostfix()
+        public static bool GameSave_SaveCurrentGame_Prefix()
         {
             PartnerPatches.UploadClusterGenerationToGalaxyServer(GameMain.data);
+            return true;
+        }
+        
+        /*
+         * 该补丁为了一定可以执行解锁成就的方法的目的
+         * 判断游戏数据是否正常
+         * @param __result = true, 游戏数据正常
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof (GameAbnormalityCheck_Obsolete), "isGameNormal")]
+        public static bool GameAbnormalityCheck_Obsolete_isGameNormal_Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
         }
     }
 }

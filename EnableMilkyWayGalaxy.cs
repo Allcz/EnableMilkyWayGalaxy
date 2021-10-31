3ea67c9383abfd741445f60c1b6d9c2c422cf82a
Allz <278137257@qq.com>
2021-10-31 12:54:08 +0800

    add

---
+++ EnableMilkyWayGalaxy.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+
+namespace EnableMilkyWayGalaxy
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    [BepInProcess(GameProcess)]
+    public class EnableMilkyWayGalaxy : BaseUnityPlugin
+    {
+        private const string ModGuid = "Allz.EnableMilkyWayGalaxy";
+        private const string ModName = "EnableMilkyWayGalaxy";
+        private const string ModVersion = "1.0.3";
+        private const string GameProcess = "DSPGAME.exe";
+        private readonly Harmony _harmony = new Harmony(ModGuid);
+
+        [Obsolete]
+        public void Start()
+        {
+            _harmony.PatchAll();
+        }
+    }
+}
